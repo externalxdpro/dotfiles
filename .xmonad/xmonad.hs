a152dbad3bbe34e405b8ae07a1c0eb1166b0cd69
Sakib Ahmed <externalxdpro@gmail.com>
2021-09-25 22:35:32 -0400

    Switch from dmenu to rofi

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -144,7 +144,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- SUPER + FUNCTION KEYS
 
   [ ((modMask, xK_f), sendMessage $ Toggle NBFULL)
-  , ((modMask, xK_p ), spawn $ "dmenu_run -i -nb '#191919' -nf '#fea63c' -sb '#fea63c' -sf '#191919' -fn 'NotoMonoRegular:bold:pixelsize=14'")
+  -- , ((modMask, xK_p ), spawn $ "dmenu_run -i -nb '#191919' -nf '#fea63c' -sb '#fea63c' -sf '#191919' -fn 'NotoMonoRegular:bold:pixelsize=14'")
+  , ((modMask, xK_p ), spawn $ "rofi -show drun -show-icons" )
   , ((modMask, xK_y), spawn $ "polybar-msg cmd toggle" )
   , ((modMask, xK_x), spawn $ "arcolinux-logout" )
   , ((modMask, xK_Escape), spawn $ "xkill" )
