42aacadb8ad4332df34173ba98ea1d0c6356e062
Sakib Ahmed <externalxdpro@gmail.com>
2021-10-25 15:21:47 -0400

    Switch default browser to firefox

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -79,7 +79,7 @@ myFileManager :: String
 myFileManager = "pcmanfm"  -- Sets pcmanfm as file manager
 
 myBrowser :: String
-myBrowser = "brave"  -- Sets brave as browser
+myBrowser = "firefox"  -- Sets firefox as browser
 
 myEmacs :: String
 myEmacs = "emacsclient -c -a 'emacs' "  -- Makes emacs keybindings easier to type
