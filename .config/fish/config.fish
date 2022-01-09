8055eb592c668090c3f7ef65bf64ede906eec53f
externalxdpro <externalxdpro@gmail.com>
2022-01-09 18:55:22 -0500

    Remove unnecessary aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -214,10 +214,6 @@ alias .5='cd ../../../../..'
 alias vim='nvim'
 alias em='/usr/bin/emacs -nw'
 alias emacs="emacsclient -c -a 'emacs'"
-alias doomsync="~/.emacs.d/bin/doom sync"
-alias doomdoctor="~/.emacs.d/bin/doom doctor"
-alias doomupgrade="~/.emacs.d/bin/doom upgrade"
-alias doompurge="~/.emacs.d/bin/doom purge"
 
 # Changing "ls" to "exa"
 alias ls='exa -al --color=always --group-directories-first' # my preferred listing
