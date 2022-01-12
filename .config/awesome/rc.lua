e70a5acf7dd827fa35f292c8bc9749675c573b67
externalxdpro <externalxdpro@gmail.com>
2022-01-11 22:05:53 -0500

    Enable picom experimental backends

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -737,7 +737,7 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 awful.spawn.with_shell("nitrogen --restore")
-awful.spawn.with_shell("picom")
+awful.spawn.with_shell("picom --experimental-backends")
 awful.spawn.with_shell("nm-applet")
 awful.spawn.with_shell("volctl")
 awful.spawn.with_shell("blueman-applet")
