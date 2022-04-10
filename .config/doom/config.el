11e776954a1ecd3aca5a78b5eeb7828a1750477f
externalxdpro <externalxdpro@gmail.com>
2022-04-10 12:41:35 -0400

    Add extra org mode configuration

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -166,4 +166,13 @@ List of keybindings (SPC h b b)")
 (after! org
   (setq org-directory "~/Notes/"
         org-agenda-files '("~/Notes/agenda.org")
-        org-log-done 'time))
+        org-log-done 'time
+        org-hide-emphasis-markers t))
+
+(custom-set-faces
+  '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+  '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+  '(org-level-4 ((t (:inherit outline-4 :height 1.1))))
+  '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+)
