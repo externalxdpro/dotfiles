eb26ef03aed58b8b8fe39849786d993456b9a2cd
externalxdpro <externalxdpro@gmail.com>
2022-12-07 12:26:06 -0500

    Add new keybindings

--- .config/nvim/lua/ext/keybinds.lua
+++
@@ -1,6 +0,0 @@
-local function map(mode, key, value)
-	vim.keymap.set(mode, key, value, { silent = true })
-end
-
--- Nerd Tree
-map('n', '<leader>op', '<CMD>NERDTreeToggle<CR>')
---
+++ .config/nvim/lua/keybinds.lua
@@ -0,0 +1,33 @@
+local opts = { noremap = true, silent = true }
+local function map(mode, key, value)
+	vim.keymap.set(mode, key, value, opts)
+end
+
+-- Base
+-- Buffers
+map('n', '<leader>bn', '<CMD>bnext<CR>')
+map('n', '<leader>bp', '<CMD>bprevious<CR>')
+map('n', '<leader>bk', '<CMD>Bclose<CR>')
+
+-- Indent
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
+-- Windows
+map('n', '<leader>wh', '<C-w>h')
+map('n', '<leader>wj', '<C-w>j')
+map('n', '<leader>wk', '<C-w>k')
+map('n', '<leader>wl', '<C-w>l')
+
+-- Other
+map('v', 'p', '"_dP')
+
+-- Plugins
+-- Vimagit
+map('n', '<leader>gg', '<CMD>Magit<CR>')
+
+-- Ranger
+map('n', '<leader>od', '<CMD>Ranger<CR>')
+
+-- Nerd Tree
+map('n', '<leader>op', '<CMD>NERDTreeToggle<CR>')
