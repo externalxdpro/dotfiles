33c675fb7f15c07183466511bbf25fb080892284
externalxdpro <externalxdpro@gmail.com>
2022-12-07 16:22:59 -0500

    Add more keybindings

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -9,10 +9,6 @@ map('n', '<leader>bn', '<CMD>bnext<CR>')
 map('n', '<leader>bp', '<CMD>bprevious<CR>')
 map('n', '<leader>bk', '<CMD>bdelete<CR>')
 
--- Indent
-map('v', '<', '<gv')
-map('v', '>', '>gv')
-
 -- Windows
 map('n', '<leader>wh', '<C-w>h')
 map('n', '<leader>wj', '<C-w>j')
@@ -20,17 +16,22 @@ map('n', '<leader>wk', '<C-w>k')
 map('n', '<leader>wl', '<C-w>l')
 map('n', '<leader>ws', '<CMD>split<CR>')
 map('n', '<leader>wv', '<CMD>vsplit<CR>')
+map('n', '<leader>w=', '<C-w>=')
 map('n', '<leader>w+', '<CMD>resize -2<CR>')
 map('n', '<leader>w-', '<CMD>resize +2<CR>')
 map('n', '<leader>w<', '<CMD>vertical resize -2<CR>')
 map('n', '<leader>w>', '<CMD>vertical resize +2<CR>')
 map('n', '<leader>wc', '<CMD>q<CR>')
 
+-- Indent
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
 -- Other
 map('v', 'p', '"_dP')
 
 -- Plugins
--- Vimagit
+-- LazyGit
 map('n', '<leader>gg', '<CMD>LazyGit<CR>')
 
 -- Ranger
