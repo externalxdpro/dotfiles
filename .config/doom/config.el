bbdc34ff00e8a493995652c7eb2219e1de89f338
externalxdpro <externalxdpro@gmail.com>
2023-07-30 11:52:33 -0400

    Fix indenting

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -212,11 +212,11 @@ List of keybindings (SPC h b b)")
    #+end_signature")
 
 (after! mu4e
-    (setq mu4e-update-interval (* 5 60)                         ;; get emails and index every 5 minutes
-        mu4e-get-mail-command "mbsync -a -c ~/.config/mbsyncrc" ;; set a custom sync command
-        mu4e-compose-format-flowed t                            ;; send emails with format=flowed
-        mu4e-index-cleanup nil                                  ;; don't do a full cleanup check
-        mu4e-index-lazy-check t))                               ;; don't consider up-to-date dirs
+  (setq mu4e-update-interval (* 5 60)                       ;; get emails and index every 5 minutes
+    mu4e-get-mail-command "mbsync -a -c ~/.config/mbsyncrc" ;; set a custom sync command
+    mu4e-compose-format-flowed t                            ;; send emails with format=flowed
+    mu4e-index-cleanup nil                                  ;; don't do a full cleanup check
+    mu4e-index-lazy-check t))                               ;; don't consider up-to-date dirs
 
 (mu4e t)        ;; check for emails in the background
 
