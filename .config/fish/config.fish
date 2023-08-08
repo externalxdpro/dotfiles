26384dfd4472504e7d6686ab2e492c0243d4bb51
externalxdpro <externalxdpro@gmail.com>
2023-08-07 23:47:02 -0400

    Disable bat for manpages

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,7 @@ set -Ux VISUAL "emacsclient -c -a emacs"          # $VISUAL use Emacs in GUI mod
 ### Uncomment only one of these!
 
 ### "bat" as manpager
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 ### "vim" as manpager
 # set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
