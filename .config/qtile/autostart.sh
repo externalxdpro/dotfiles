106934fc1a41182e97f89241cd825d7730c9a657
externalxdpro <externalxdpro@gmail.com>
2025-04-15 12:31:08 -0400

    Add autostart programs for qtile

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
 
-kanshi &
+$HOME/.screenlayout.sh &    # for x11
+kanshi &                    # for wayland
+nitrogen --restore &
+/usr/bin/lxpolkit &
+dunst -conf $HOME/.config/dunst/qtilerc &
+picom &
 /usr/bin/emacs --daemon &
-nm-applet &
-lxsession &
+numlockx on &
+nm-applet --indicator &
+blueman-applet &
+udiskie &
 dex -a -s .config/autostart &
+
