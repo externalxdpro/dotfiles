bec56a68de041617f3b25a5c886611791fb52944
externalxdpro <externalxdpro@gmail.com>
2025-09-03 21:33:09 -0400

    Enable openvpn networkmanager plugin

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -30,7 +30,10 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [ networkmanager-openvpn ];
+  };
 
   # Enable bluetooth
   hardware.bluetooth = {
