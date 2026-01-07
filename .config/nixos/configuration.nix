c8c98ac2f1d17e4a4f841f34e583c74592aa375c
externalxdpro <externalxdpro@gmail.com>
2026-01-07 17:07:20 -0500

    Add libraries for appimage-run for fladder

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -189,6 +189,13 @@
   programs.appimage = {
     enable = true;
     binfmt = true;
+    package = pkgs.appimage-run.override {
+      extraPkgs =
+        pkgs: with pkgs; [
+          mpv
+          libepoxy
+        ];
+    };
   };
 
   programs.direnv.enable = true;
