b87261973b4af35b4c1bbced950b47b97b4ed621
externalxdpro <externalxdpro@gmail.com>
2026-02-01 19:26:20 -0500

    Rename kanshi config

--- .config/river/autostart.sh
+++ .config/river/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-kanshi -c ~/.config/kanshi/riverconf &
+kanshi &
 #swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' &
 #sway-audio-idle-inhibit &
 waybar -c ~/.config/waybar-river/config -s ~/.config/waybar-river/style.css &
