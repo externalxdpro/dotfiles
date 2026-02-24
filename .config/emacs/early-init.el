5f27f56dbab27f01679578c20e6c4d0f9fd48a88
externalxdpro <externalxdpro@gmail.com>
2026-02-23 19:01:51 -0500

    Add some performance settings

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,2 +1,18 @@
 (setq package-enable-at-startup nil)
 
+;; Performance settings
+(setq gc-cons-threshold 100000000) ;; 100 MB
+
+;; Improve performance with language servers.
+(setq read-process-output-max (* 1024 1024)) ;; 1 MB
+
+;; Improve tree-sitter performance
+(setenv "LSP_USE_PLISTS" "true")
+(setq lsp-use-plists t)
+
+;; Make scratch buffer initial buffer
+(setq inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-buffer-menu t)
+
